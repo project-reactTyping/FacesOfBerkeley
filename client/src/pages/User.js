41562a27eb1fbcc43c23724c3e06f4f7b396f942
Mark Bui <tvyybui@yahoo.com>
2017-09-23 16:23:48 -0700

    built out skeleton react app, home page loads!

---
+++ client/src/pages/User.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class User extends Component {
+
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  loadUsers = () => {
+
+  };
+
+  render() {
+    return (
+      <h1>This is a test</h1>
+    )
+  };
+}
+
+export default User;

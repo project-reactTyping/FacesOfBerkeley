60781cc765e38e417ec26a5a43f25006c1da24fb
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2017-10-02 12:16:07 -0700

    fix merge error in Calendar

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -11,13 +11,8 @@ class Calendar extends React.Component {
 	render() {
 		return (
 		  <InfiniteCalendar
-<<<<<<< HEAD
 		    width={250}
 		    height={400}
-=======
-		    width={275}
-		    height={350}
->>>>>>> 043f1b3fa6abd1e1a0a8c528a361b8d722a1cf7b
 		    selected={today}
 		    minDate={lastWeek} />
 		);
